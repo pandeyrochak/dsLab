8f9a31358eccf9ba6ac29b18ce6f326d27c69ba6
pandeyrochak <rochakpandey56@gmail.com>
2020-02-21 12:19:24 +0530

    -a

---
+++ dataframe.R
@@ -0,0 +1,46 @@
+rm(list=ls())
+#q1
+age<- c(22,25,26,20)
+name<-c("James","Mathew","Olivia","Stella")
+gender<-c("M","M","F","F")
+df<-data.frame(age,name,gender)
+(df[1:2,])
+
+#q2
+df1<-df[,c("name","age","gender")]
+df1
+
+#q3
+df1<-df1[,c("gender","age","name")]
+df1
+df1<-df1[,c("age","gender","name")]
+
+#q4
+df2<-df
+df2$age<-NULL
+df2
+
+#q5
+
+names(df1)[1] <- "FirstName"
+df1
+
+#q6
+df3<-df[order(df$age,decreasing = TRUE),]
+df3
+df3[which.min(df3$age),]
+
+#q7
+numeric_cols<-(iris[sapply(iris, is.numeric)])
+numeric_cols
+
+#q8
+print(tapply(iris$Sepal.Length,iris$Species, mean))
+print(tapply(iris$Sepal.Width,iris$Species, mean))
+print(tapply(iris$Petal.Length,iris$Species, mean))
+print(tapply(iris$Petal.Width,iris$Species, mean))
+
+#Extended Practical List Questions
+#q17
+sapply(numeric_cols,mean)
+#q22
