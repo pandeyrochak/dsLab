b131aa4e2aeb3b86624203701d25deac30528ea5
pandeyrochak <rochakpandey56@gmail.com>
2020-03-04 12:47:52 +0530

    ...

---
+++ programmingConstruct.R
@@ -0,0 +1,84 @@
+#---q3---
+n <- as.integer(readline(prompt="Enter a number: "))
+cat("Sum of numbers upto",n,"is",sum(1:n))
+
+#---q4---
+for(i in 1:12)
+{
+  cat("Table of ",i,"\n")
+  for(j in 1:10)
+  {
+    cat(i*j,"\n")
+  }
+}
+
+#---q5---
+getMax<- function(x){
+  return(max(unlist(x)))
+}
+myList<-list(100,34,534,987,1,8)
+print(getMax(myList))
+
+#---q6---
+
+
+
+#---q8---
+linearSearch<-function(key,n){
+  flag<-0
+    pos<-1
+  for(i in n){
+    if(i==key){
+      flag<-1
+      break
+    }
+    pos<-pos+1
+  }
+  if(flag==1){
+    cat("Element Found at ",pos," position")
+  }else{print("Not Found")}
+}
+n <- c(34,78,234,768,123,76,34,3,1)
+key<-as.integer(readline(prompt = "Enter Element to search: "))
+linearSearch(key,n)
+
+
+#---q9---
+binarySearch<-function(f,l,key,n){
+  mid=as.integer((f+l)/2)
+  if(f>l){
+    return(-1)
+  }
+  if(n[mid]==key){
+    return(mid)
+  }
+  if(n[mid]>key){
+    l<-mid-1
+  }else{
+    f<-mid+1
+  }
+  binarySearch(f,l,key,n)
+}
+n <- c(34,78,234,768,123,76,39,3,1)
+key<-as.integer(readline(prompt = "Enter Element to search: "))
+n<-sort(n,decreasing = FALSE)
+res<-binarySearch(1,length(n),key,n)
+if(res!=-1){
+  cat("Element found at ",res)
+}else{
+  print("Element not found!!!")
+}
+
+
+#---q18---
+check<-function(x){
+  if(!is.finite(x)){
+    return(NA)
+  }
+  if(x %% 2 == 0){
+    return(TRUE)
+  }else{
+    return(FALSE)
+  }
+}
+sapply(c(1:5,Inf,-Inf,NA),check)
