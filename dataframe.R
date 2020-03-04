f91faf8bfc8407b1559a6d0142bf39e05dab182d
pandeyrochak <rochakpandey56@gmail.com>
2020-03-04 12:44:49 +0530

    ...

--- dataframe.R
+++ dataframe.R
@@ -1,46 +1,49 @@
 rm(list=ls())
-#q1
+#---q1
 age<- c(22,25,26,20)
 name<-c("James","Mathew","Olivia","Stella")
 gender<-c("M","M","F","F")
 df<-data.frame(age,name,gender)
 (df[1:2,])
 
-#q2
+#---q2
 df1<-df[,c("name","age","gender")]
 df1
 
-#q3
+#---q3
 df1<-df1[,c("gender","age","name")]
 df1
 df1<-df1[,c("age","gender","name")]
 
-#q4
+#---q4
 df2<-df
 df2$age<-NULL
 df2
 
-#q5
+#---q5
 
 names(df1)[1] <- "FirstName"
 df1
 
-#q6
+#---q6
 df3<-df[order(df$age,decreasing = TRUE),]
 df3
 df3[which.min(df3$age),]
 
-#q7
+#---q7
 numeric_cols<-(iris[sapply(iris, is.numeric)])
 numeric_cols
 
-#q8
+#---q8
 print(tapply(iris$Sepal.Length,iris$Species, mean))
 print(tapply(iris$Sepal.Width,iris$Species, mean))
 print(tapply(iris$Petal.Length,iris$Species, mean))
 print(tapply(iris$Petal.Width,iris$Species, mean))
 
 #Extended Practical List Questions
-#q17
-sapply(numeric_cols,mean)
-#q22
+#---q17
+val<-sapply(numeric_cols,mean)
+write.csv2(val,"./Documents/ds_wd/file.csv",row.names = TRUE)
+
+
+#---q22
